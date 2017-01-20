859113d9b52061c447cbf757209fafc7a6f478e7
Unknown <benevedeselias@gmail.com>
2017-01-20 13:45:24 -0800

    Added guessing game
    Allows concurrency for games (and other commands) across multiple servers/channels/users

--- butler.py
+++ butler.py
@@ -1,4 +1,4 @@
-import discord, random, asyncio, pickle, os, os.path, re
+import discord, random, asyncio, pickle, os, os.path, re, random
  
 client = discord.Client()
 settings = {}
@@ -16,6 +16,36 @@ def isAuthed(message, settings, server):
             if i == j.name:
                 return True
     return False
+   
+guessingGames = {}
+class GuessingGame():
+    def __init__(self, user, client, message):
+        self.user = user
+        self.number = random.randint(1, 20)
+        self.tries = 0
+        self.client = client
+        self.channel = message.channel
+        client.loop.create_task(self.sendMessage("Game Started!", self.channel))
+
+    def parseMessage(self, message, command):
+        if(message.author.id == self.user and message.channel == self.channel):
+            guessed = int(command[1])
+            if(command[0] == "guess"):
+                if guessed > 20 or guessed < 1:
+                    client.loop.create_task(self.sendMessage("Please enter a number between 1-20.", message.channel))
+                elif guessed < self.number:
+                    self.tries += 1
+                    client.loop.create_task(self.sendMessage("Too low! Total Tries: " + str(self.tries), message.channel))
+                elif guessed > self.number:
+                    self.tries += 1
+                    client.loop.create_task(self.sendMessage("Too high! Total Tries: " + str(self.tries), message.channel))
+                elif guessed == self.number:
+                    guessingGames.pop(message.author.id + message.channel.id, None)
+                    client.loop.create_task(self.sendMessage("You got it right! Total tries: " + str(self.tries), message.channel))
+
+    @asyncio.coroutine
+    def sendMessage(self, message, channel):
+        yield from self.client.send_message(channel, message)
    
 @client.event
 @asyncio.coroutine
@@ -40,55 +70,66 @@ def on_ready():
 @asyncio.coroutine
 def on_message(message):
     global settings
-	
+    
     if message.server.id not in servers:
         servers.append(message.server.id)
         settings[message.server.id] = {'commands': [], 'authedRoles': [], 'data': {}}
         saveSettings(settings, message.server.id)
     server = message.server.id
-	
+    
     print("[" + message.server.name + "]:[" + message.channel.name + "] <" + message.author.name + ">: " + re.sub(r'[^\x00-\x7F]+','{emoji}', message.content))
-	
+    
     if message.content.startswith("!"): #If its a command
         if(message.author.name != "Butler"): #If its not from me
             message.content = message.content[1:] #Remove the !
             word = message.content.split(" ") #Split up all the words into a list
-			
+            
             for i in settings[server]['commands']: #Execute all text commands
                 if(word[0] == i[0]):
                     yield from client.send_message(message.channel, i[1])
-					
+                    
             if(word[0] == "newCommand"): #For a new listed command
                 if(isAuthed(message, settings, server)):
                     settings[server]['commands'].append([word[1],' '.join(word[2:])]) #Add the new command with all the text after concatenated with spaces
                     saveSettings(settings, server)
                     yield from client.send_message(message.channel, "Command " + word[1] + " added!")
-					
+                    
             elif(word[0] == "list"): #List all the commands
                 temp = "Commands:\n"
                 for i in settings[server]['commands']:
                     temp = temp + i[0] + ": " + i[1] + "\n" #commandName: commandText
                 yield from client.send_message(message.channel, temp)
-					
+                    
             elif(word[0] == "delCommand"):
                 if(isAuthed(message, settings, server)):
                     for i in settings[server]['commands']:
                         if(i[0] == word[1]):
                             settings[server]['commands'].remove(i)
                             yield from client.send_message(message.channel, "Command " + i[0] + " removed.")
-							
+                            
             elif(word[0] == "addAuthRole"): #Add new auth role
                 if(isAuthed(message, settings, server)):
                     settings[server]['authedRoles'].append(''.join(word[1:]))
                     saveSettings(settings, server)
                     yield from client.send_message(message.channel, "Added " + ''.join(word[1:]) + " to authenticated roles.")
-					
+                    
             elif(word[0] == "privilege"):
                 if(not isAuthed(message, settings, server)):
                     yield from client.send_message(message.channel, "You are a normal user, no admin operations may be executed from your account.")
                 else:
                     yield from client.send_message(message.channel, "You may execute any command available on this server.")
-
+            
+            elif(word[0] == "newGame"):
+                if((message.author.id + message.channel.id) in guessingGames):
+                    yield from client.send_message(message.channel, "You already have a game running! Guess with !guess.")
+                else:
+                    guessingGames[message.author.id + message.channel.id] = GuessingGame(message.author.id, client, message)
+            
+            elif(word[0] == "guess"):
+                if((message.author.id + message.channel.id) in guessingGames):
+                    guessingGames[message.author.id + message.channel.id].parseMessage(message, word)
+                else:
+                    yield from client.send_message(message.channel, "Start a game with !newGame first!")
 
     elif(message.content.lower().startswith("hello butler")): #Response to a nice hello
         if(not message.channel.is_private):
